b38d4a3a952f143358c6ee526777f75d0b6bf90a
Christopher Hammond <christopherhammond13@gmail.com>
2019-01-29 13:53:24 +0000

    Writes the latest build's filename to /builds/LATEST_BUILD

--- get_build.sh
+++ get_build.sh
@@ -2,7 +2,10 @@
 [ -f /builds/.keep ] || (echo "/builds volume not mounted!"; exit 1)
 
 timestamp=`date "+%Y-%m-%dT%H%M%SZ" -u`
-output_path=/builds/nginx-build_${timestamp}.tar.gz
+filename=nginx-build_$timestamp.tar.gz
+output_path=/builds/$filename
 cp /etc/nginx-build.tar.gz $output_path
+
+echo $filename > /builds/LATEST_BUILD
 
 echo $output_path
